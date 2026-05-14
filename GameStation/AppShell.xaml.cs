a2d77c5ef52ef7baa2e232f5aaf467c5c7b06a95
Antonin Dumoutier <antonin.dumoutier@gmail.com>
2026-05-14 11:21:02 +0200

    Initial Commit !

---
+++ GameStation/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GameStation
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GameStation/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace GameStation
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+            languagePicker.SelectedIndex = 0;
+        }
+
+        private void OnLanguageChange(object sender, EventArgs e)
+        {
+            return;
+        }
+
+    }
+}
